d6f2708b33e05d078d02a05efa414de0d0f1f980
Subrat Pandey <subratpandey974@gmail.com>
2024-04-22 23:31:19 -0500

    Added achievements and hobbies section

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -15,19 +15,41 @@ function AboutCard() {
             A passionate developer, I am a 2X software engineering intern at Google.
             <br />
             <br />
-            Besides coding, some other activities that I love to do!
           </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Outdoor Sports
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Writing Poetry and Non-Fiction
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling
-            </li>
-          </ul>
+
+          <div className="achievements-section">
+            <span className="purple">Notable Achievements</span><br /><br />
+            <ul>
+              <li>Undergrad GPA: 3.94/4.0</li>
+              <li>U.S National Champion at HBCU Battle of the Brains 2024</li>
+              <li>2nd Position at Equinix Fisk Hackathon</li>
+              <li>2nd Position at Zillow HBCU Hackathon</li>
+              <li>Founder at Student Coalition for Nepal</li>
+              <li>Ambassador for HomefreeUSA's Center for Financial Advancement</li>
+              <li>Expedition EY Scholar</li>
+              <li>President of Global Peace Young Leaders</li>
+              <li>IBM Z Ambassador</li>
+            </ul>
+          </div>
+          <br></br>
+
+          <div className="Hobbies">
+            <span className="purple">Interests and Hobbies</span><br /><br />
+            <ul>
+              <li className="about-activity">
+                <ImPointRight /> Playing Outdoor Sports
+              </li>
+              <li className="about-activity">
+                <ImPointRight /> Writing Poetry and Non-Fiction
+              </li>
+              <li className="about-activity">
+                <ImPointRight /> Mixed-Martial Arts
+              </li>
+              <li className="about-activity">
+                <ImPointRight /> Travelling
+              </li>
+            </ul>
+          </div>
         </blockquote>
       </Card.Body>
     </Card>
