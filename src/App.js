5ba66f0790f09ebcff049baef48f12b40908ac8e
Subrat Pandey <subratpandey974@gmail.com>
2025-03-19 17:50:28 -0500

    removed router to Blog section

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
-import Blog from "./components/Blog/Blog";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import {
@@ -40,7 +39,6 @@ function App() {
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
-          <Route path="/blog" element={<Blog />} />
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
         <Footer />
