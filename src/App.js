84b9b62de918c390411c4b6da0f7bd5839f9e90d
Subrat Pandey <subratpandey974@gmail.com>
2025-03-19 21:09:59 -0500

    updated router for certificate page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
+import Certifications from "./components/Certifications/Certifications";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import {
@@ -39,6 +40,7 @@ function App() {
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
+          <Route path="/certifications" element={<Certifications />} />
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
         <Footer />
